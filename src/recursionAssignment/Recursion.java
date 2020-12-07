9158b1279a27b52b8e21702700870d2568824948
Kyle Mendes <kyle.mendes21@ycdsbk12.ca>
2020-12-07 21:47:19 +0000

    count7 recursion line 2

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -7,13 +7,19 @@ public class Recursion{
     //Basics Line
     if (n == 0) {
       return 0;
-    
+    }
     // recursion line 1
-    }else if (n % 10 == 7) {
+    if (n % 10 == 7) {
       return 1 + count7(n / 10);
     }
 
-    return 0;
+    // recursion line 2
+    else {
+      return count7(n / 10);
+    }
+
+    
+
   }
 
 }
